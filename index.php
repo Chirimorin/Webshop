b48f55bba5de082d6ef23a6ca9be09a3b9edb6ec
Thomas Voogt <chirimorin@gmail.com>
2014-02-22 12:47:29 +0100

    initial commit
    + basic setup

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	session_start();
+	include("layout/header.php");
+	include("layout/menu.php");
+	
+	if (isset($_GET['page']))
+	{
+		if (file_exists("controllers/".$_GET['page'].".php"))
+		{
+			include("controllers/".$_GET['page'].".php");
+		}
+		else
+		{
+			include("controllers/home.php");
+		}
+	}
+	else
+	{
+		include("controllers/home.php");
+	}
+	include("layout/footer.php");
+?>
