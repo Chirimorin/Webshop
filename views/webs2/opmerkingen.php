957f127598454138b0e14a7985ec8db8ceb3e76e
Thomas Voogt <chirimorin@gmail.com>
2014-04-04 19:51:49 +0200

    opmerkingen

--- views/webs2/opmerkingen.php
+++ views/webs2/opmerkingen.php
@@ -4,3 +4,22 @@
     <h1>Opmerkingen</h1>
 </div>
 
+<table class="table table-bordered">
+    <tbody>
+        <tr>
+            <td>De zoekfunctie gebruikt jQuery en zoekt dus niet direct uit de database.<br />Dit is goedgekeurd door de leraar.</td>
+        </tr>
+        <tr>
+            <td>jQuery word ook gebruikt voor de winkelwagen (deze word herladen met jQuery als er een aanpassing is)</td>
+        </tr>
+        <tr>
+            <td>Items in de winkelwagen kunnen verwijderd worden door het aantal op 0 te zetten.</td>
+        </tr>
+        <tr>
+            <td>Er kan worden ingelogd met admin/admin, zoals in de opdracht beschreven staat</td>
+        </tr>
+        <tr>
+            <td>Het uploaden/aanpassen van plaatjes is helaas niet meer op tijd gelukt omdat de instructies erg laat waren.</td>
+        </tr>
+    </tbody>
+</table>
